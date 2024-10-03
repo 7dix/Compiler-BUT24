c3e1f5dd97651e26be5f634bbecd817a9cafd02b
Otas02CZ <koci.ota02@gmail.com>
2024-10-03 19:33:54 +0200

    edit ret vals, added comment to shared.h

--- src/shared.h
+++ src/shared.h
@@ -11,20 +11,29 @@
 #ifndef H_DEFINITIONS
 #define H_DEFINITIONS
 
+// RETURN VALUES DEFINITIONS
 typedef enum {
-    
-    OK = 0,
-    LEXICAL_ERROR = 1,
-    SYNTAX_ERR = 2,
-    SEMANTIC_UNDEFINED_ERR = 3,
-    SEMANTIC_FUNCTION_ERR = 4,
-    SEMANTIC_REDEF_OR_BAD_ASSIGN_ERR = 5,
-    SEMANTIC_EXPRESSION_ERR = 6,
-    SEMANTIC_TYPE_COMPATIBILITY_ERR = 7,
-    SEMANTIC_TYPE_DERIVATION_ERR = 8,
-    SEMANTIC_UNUSED_VAR_ERR = 9,
-    SEMANTIC_OTHER_ERR = 10,
-    INTERNAL_ERR = 99
+    RET_VAL_OK = 0,
+    RET_VAL_LEXICAL_ERR = 1,
+    // Wrong syntax, missing header
+    RET_VAL_SYNTAX_ERR = 2,
+    // Undefined function or variable
+    RET_VAL_SEMANTIC_UNDEFINED_ERR = 3,
+    // Function call (wrong type or number of params), return value (wrong type or not allowed discard)
+    RET_VAL_SEMANTIC_FUNCTION_ERR = 4,
+    // Redefinition of variable or function, assignment to constant
+    RET_VAL_SEMANTIC_REDEF_OR_BAD_ASSIGN_ERR = 5,
+    // Missing or too many expressions in function return
+    RET_VAL_SEMANTIC_FUNC_RETURN_ERR = 6,
+    // Wrong type compat in arithmetic, string and/or relation expr., incompatible type during assignment
+    RET_VAL_SEMANTIC_TYPE_COMPATIBILITY_ERR = 7,
+    // Type is not stated and cannot be derived from the expression
+    RET_VAL_SEMANTIC_TYPE_DERIVATION_ERR = 8,
+    // Unused variable, modifiable var without possibility of change after init
+    RET_VAL_SEMANTIC_UNUSED_VAR_ERR = 9,
+    RET_VAL_SEMANTIC_OTHER_ERR = 10,
+    // Internal errors of the compiler -> insufficient memory, etc.
+    RET_VAL_INTERNAL_ERR = 99
 };
 
 
