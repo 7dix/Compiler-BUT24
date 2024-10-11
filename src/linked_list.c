0ae9158628e19607a95bb05a35ae10aaac29cc63
Otakar Kočí <xkocio00@stud.fit.vutbr.cz>
2024-10-11 22:46:40 +0200

    create files with headers for future work on data structures

---
+++ src/linked_list.c
@@ -0,0 +1,8 @@
+// FILE: linked_list.c
+// PROJECT: IFJ24 - Compiler for the IFJ24 language @ FIT BUT 2BIT
+// TEAM: Martin Zůbek (253226)
+// AUTHORS:
+//  <Otakar Kočí> (xkocio00)
+//
+// YEAR: 2024
+// NOTES:
