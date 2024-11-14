5e0a0e0e5eca50d1251e53358a7a9662ebf2f15f
Otakar Kočí <koci.ota02@gmail.com>
2024-11-14 14:36:09 +0100

    fix: change incorrect login of team leader

--- src/scanner.c
+++ src/scanner.c
@@ -1,6 +1,6 @@
 // FILE: scanner.c
 // PROJECT: IFJ24 - Compiler for the IFJ24 language @ FIT BUT 2BIT
-// TEAM: Martin Zůbek (253226)
+// TEAM: Martin Zůbek (253206)
 // AUTHORS:
 //  <Marek Tenora> (xtenor02)
 //
