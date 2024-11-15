0bdfd3e3d2157df2ac502f1edd22a79add1ecdc1
Otakar Kočí <xkocio00@stud.fit.vutbr.cz>
2024-11-15 11:56:03 +0100

    rm: remove empty files for ds
    needed data structures will be added when needed

--- src/ast.h
+++
@@ -1,8 +0,0 @@
-// FILE: ast.h
-// PROJECT: IFJ24 - Compiler for the IFJ24 language @ FIT BUT 2BIT
-// TEAM: Martin Zůbek (253226)
-// AUTHORS:
-//  <Otakar Kočí> (xkocio00)
-//
-// YEAR: 2024
-// NOTES:
--- src/linked_list.h
+++
@@ -1,8 +0,0 @@
-// FILE: linked_list.h
-// PROJECT: IFJ24 - Compiler for the IFJ24 language @ FIT BUT 2BIT
-// TEAM: Martin Zůbek (253226)
-// AUTHORS:
-//  <Otakar Kočí> (xkocio00)
-//
-// YEAR: 2024
-// NOTES:
