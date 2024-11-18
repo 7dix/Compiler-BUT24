8ea9f6788e8e1aab5bad917be0d5cb82803f3ea7
7dix <tenoramarek@gmail.com>
2024-11-18 21:02:22 +0100

    feat: added hashtable datatype

---
+++ src/symtable.c
@@ -0,0 +1,10 @@
+// FILE: symtable.c
+// PROJECT: IFJ24 - Compiler for the IFJ24 language @ FIT BUT 2BIT
+// TEAM: Martin Zůbek (253206)
+// AUTHORS:
+//  <Marek Tenora> (xtenor02)
+// <Otakar Kočí> (xkocio00)
+//
+// YEAR: 2024
+
+#include "symtable.h"
--- src/symtable.h
+++ src/symtable.h
@@ -1,3 +1,12 @@
+// FILE: symtable.h
+// PROJECT: IFJ24 - Compiler for the IFJ24 language @ FIT BUT 2BIT
+// TEAM: Martin Zůbek (253206)
+// AUTHORS:
+//  <Marek Tenora> (xtenor02)
+// <Otakar Kočí> (xkocio00)
+//
+// YEAR: 2024
+
 #ifndef H_SYMTABLE
 #define H_SYMTABLE
 
@@ -50,5 +59,23 @@ typedef struct Symbol {
     bool used;
     struct Symbol *next; // For handling collisions in the hashtable
 } Symbol;
+
+//-----------------------------------HASH TABLE-----------------------------------//
+
+#define HASHTABLE_SIZE 2000
+
+// Hashtable structure
+typedef struct {
+    Symbol *table[HASHTABLE_SIZE]; // Array of entry pointers
+    int count; // Tracks number of entries
+} Hashtable;
+
+// Hashtable functions
+Hashtable *hashtable_init();
+void hashtable_free(Hashtable *ht);
+Symbol *hashtable_insert(Hashtable *ht, const char *key, SymbolType type, SymbolData data);
+Symbol *hashtable_find(Hashtable *ht, const char *key);
+void hashtable_remove(Hashtable *ht, const char *key);
+
 
 #endif // H_SYMTABLE
